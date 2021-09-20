3f395945b2b6bf1f805fe4aad754201baca7db56
KanishkKalyani <kanishkkalyani@gmail.com>
2021-09-20 23:42:52 +0530

    Basic setup along with User, Product and ProductImage Schemas

---
+++ schemas/Product.ts
@@ -0,0 +1,37 @@
+import { list } from '@keystone-next/keystone/schema';
+import { integer, select, text, relationship } from '@keystone-next/fields';
+
+export const Product = list({
+  // access:
+  fields: {
+    name: text({ isRequired: true }),
+    description: text({
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'Unvailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        // createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer({ isRequired: true }),
+    // Can add photo later
+  },
+});
